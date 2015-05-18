78c87dd238fb177d9e26bdc5616d2c6f5fb139a8
ksjogo <jogo@kronberger-spiele.de>
2015-05-18 17:16:29 +0200

    Removed now unnecessary config

--- Classes/Server.php
+++ Classes/Server.php
@@ -5,32 +5,20 @@ class Server {
     
     private $client = null;
 
-    function __construct() {       
-        // $path = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('gateannotator') . 'Resources/Private/guzzle.phar';
-        // require_once($path);
-
-        $this->client = new \GuzzleHttp\Client(
-            [
-                'base_url' => "http://gate:8089/",
-                'defaults' => [
-                    'query'   => [
-                        //'annotationSetName' => 'Shef',
-                        'annotationType' => 'Mention',
-                        'featureName' => 'inst',
-                        'gate.export.format' => 'gate.corpora.export.HTMLOutput'
-                    ],
-                ]]);
+    function __construct()
+    {
+        $this->client = new \GuzzleHttp\Client(['base_url' => "http://gate:8089/"]);
     }
 
-    function annotate($text,$table,$uid) {
-        $ret = $this->client->get('/ggws/service', ['query' => [
-            'text' => $text,
-            'gate.mimir.uri' => "http://localhost:8081/typo3/alt_doc.php?edit[$table][$uid]=edit"
+    function annotate($text,$table,$uid)
+    {
+        $ret = $this->client->get('/gate/service', ['query' => [
+            'text' => $text
+            //'gate.mimir.uri' => "http://localhost:8081/typo3/alt_doc.php?edit[$table][$uid]=edit"
         ]]);
-        //force mimir to annotate immediately
-        // xdebug_break();
+        //force mimir to annotate
         // $resync = (string) $this->client->get('/mimir-cloud-5.1-SNAPSHOT/admin/actions/78f58cf4-5fd7-4093-aedb-d0d67c4a9c60/sync', ['auth' => ['admin', 'admin']]);    
-        return (string) $ret;
+        return (string) $ret->getBody();
     }
 
     /**
@@ -42,24 +30,6 @@ class Server {
     public function someFunction($input,$table,$uid)
     {
         $annotated = $this->annotate($input,$table,$uid);
-
-        preg_match("/<body.*\/body>/s", $annotated, $body);
-
-        $xml = new \DOMDocument();
-        libxml_use_internal_errors(true);
-        $xml->loadHTML($body[0]);
-
-        $xslt = new \DOMDocument();
-        $xslt->load(\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('gateannotator') . 'Resources/Private/transform.xslt');
-
-        $proc = new \XSLTProcessor();
-        $proc->importStylesheet($xslt);
-        $result = $proc->transformToDoc($xml);
-
-        $string = $result->saveXML();
-
-        preg_match("/<body.*?>(.*)<\/body>/", $annotated, $body);
-
-        return $body[1];
+        return $annotated;
     }
 }
